0a0dc3483665fd70d5f789b67e266b08d0ad9dc9
Nozomi <nozomi@nozomi.local>
2019-04-04 23:31:54 +0900

    Add slim

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'roots#top'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
+
 end
