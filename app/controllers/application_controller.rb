4da4f4eebe124d4e46ca91237039073348c8fc8f
Nozomi <nozomi@nozomi.local>
2019-04-04 23:59:08 +0900

    Add devise login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+
+  # ログイン後、ログアウト後のリダイレクト先指定
   def after_sign_in_path_for(resource)
     root_path
   end
-  
   def after_sign_out_path_for(resource)
     new_user_session_path
+  end
+  
+  # user_nameカラム追加設定
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_04_141615) do
+ActiveRecord::Schema.define(version: 2019_04_04_144939) do
 
   create_table "calendar_events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -69,8 +69,14 @@ ActiveRecord::Schema.define(version: 2019_04_04_141615) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
