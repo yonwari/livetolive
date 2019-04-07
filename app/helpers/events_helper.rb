d0d0fbc16b12ba3fc1f06444bc774e33f3ceba9f
Nozomi <nozomi@nozomi.local>
2019-04-07 20:30:26 +0900

    Add tag ransack feature

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
   # 芸人タグをリンク化
   def render_with_hashtags(content)
-    content.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/events/comedian/#{word.delete("#")}"}.html_safe
-  end 
+    content.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, { controller: :events, action: :index, q:{ comedianlist_cont: :"#{word}" }} }.html_safe
+  end
 end
