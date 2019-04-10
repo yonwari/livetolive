462efc74b681cd3b10fe3be44bf211d3b63224f9
Nozomi <nozomi@nozomi.local>
2019-04-10 11:47:56 +0900

    Update search feature in TOP

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -1,5 +1,14 @@
 class RootsController < ApplicationController
   def top
+    #検索画面用
+    @search = Event.from_now.ransack(params[:q])
+
+    #人気ライブ表示用
+    @pop_events = Event.select('events.*', 'count(calendar_events.id) AS calendars')
+    .left_joins(:calendar_events)
+    .group('events.id')
+    .limit(10)
+    .order('calendars desc')
   end
 
   def admin_top
