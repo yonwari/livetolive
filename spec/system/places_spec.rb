3b2db7dc15f2a377152a7a2ec464f322deee769b
Nozomi <nozomi@nozomi.local>
2019-04-10 17:18:35 +0900

    Fix place spec

--- spec/system/places_spec.rb
+++ spec/system/places_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 describe 'ライブ会場管理機能', type: :system do
-  let(:user) { FactoryBot.create(:user)}
-  let(:admin_user) { FactoryBot.create(:admin_user)}
-  FactoryBot.create(:place)
+  let(:user) { FactoryBot.create(:user) }
+  let(:admin_user) { FactoryBot.create(:admin_user) }
+  let(:test_place) { FactoryBot.create(:place) }
 
   before do
     # ログイン
     visit new_user_session_path
-    fill_in 'Email', with: login_user.email
-    fill_in 'Password', with: login_user.password
-    click_button 'Sign in'
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログイン'
     visit places_path
   end
 
@@ -33,6 +33,23 @@ describe 'ライブ会場管理機能', type: :system do
   end
 
 
+  describe '詳細表示機能' do
+    context '管理者でログインしている時' do
+      let(:login_user) { admin_user }
+
+      before do
+        visit place_path(test_place)
+      end
+
+      it 'ライブ会場詳細が表示される' do
+        expect(page).to have_content 'テスト会場'
+        expect(page).to have_content '東京都練馬区下石神井'
+        expect(page).to have_content '35'
+        expect(page).to have_content '139'
+      end
+    end
+  end
+
   describe '新規作成機能' do
     let(:login_user) { admin_user }
 
@@ -52,17 +69,51 @@ describe 'ライブ会場管理機能', type: :system do
       end
     end
 
-    context '新規作成画面で会場名と住所を入力しなかった時' do
+    context '新規作成画面で情報を入力しなかった時' do
       let(:place_name) { '' }
       let(:address) { '' }
 
       it 'エラーとなる' do
-        # within '#error_explanation' do
-          expect(page).to have_content 'error'
-        # end
+        within '#error_explanation' do
+          expect(page).to have_content '会場名を入力してください'
+          expect(page).to have_content '住所を入力してください'
+        end
       end
     end
   end
 
 
+
+
+  describe '編集機能' do
+    let(:login_user) { admin_user }
+
+    before do
+      visit edit_place_path( test_place )
+      fill_in '会場名', with: place_name
+      fill_in '住所', with: address
+      click_button '更新'
+    end
+
+    context '編集画面で会場名と住所を入力した時' do
+      let(:place_name) { '新宿バティオス' }
+      let(:address) { '東京都新宿区歌舞伎町２丁目４５−４' }
+
+      it '正常に登録される' do
+        expect(page).to have_content '新宿バティオス'
+      end
+    end
+
+    context '編集画面で情報を削除した時' do
+      let(:place_name) { '' }
+      let(:address) { '' }
+
+      it 'エラーとなる' do
+        within '#error_explanation' do
+          expect(page).to have_content '会場名を入力してください'
+          expect(page).to have_content '住所を入力してください'
+        end
+      end
+    end
+  end
 end
