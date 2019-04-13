567bcc8e319f810e280f1855a235ae539b29b202
Nozomi <nozomi@nozomi.local>
2019-04-13 23:54:51 +0900

    Update favorite to non-ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,11 +4,15 @@ class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.new(event_id: @event.id)
     favorite.save
+    flash[:notice] = "#{@event.event_title}をお気に入りに登録しました"
+    redirect_to user_path(current_user)
   end
 
   def destroy
     favorite = current_user.favorites.find_by(event_id: @event.id)
     favorite.destroy
+    flash[:notice] = "#{@event.event_title}をお気に入りから削除しました"
+    redirect_to user_path(current_user)
   end
   
   private
