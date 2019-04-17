4a9c6f174d6a2791475cd22e95042e0e1ac1094f
Nozomi <nozomi@nozomi.local>
2019-04-17 19:23:16 +0900

    Update rails_helper to set host

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
