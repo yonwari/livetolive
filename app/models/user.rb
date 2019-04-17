6a524227e0887e6c9e0a631455e504adfc8be6c4
Nozomi <nozomi@nozomi.local>
2019-04-17 14:13:43 +0900

    Clean google login codes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   #関連付け
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :favorites, dependent: :destroy
   has_many :calendar_events, dependent: :destroy
   has_many :events, through: :calendar_events
@@ -14,16 +14,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
-  # omniauthのコールバック時に呼ばれるメソッド
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.uid =      auth.uid,
-  #     user.provider =  auth.provider,
-  #     user.email = auth.info.email
-  #     user.user_name = auth.info.name
-  #     user.password = Devise.friendly_token[0,20]
-  #   end
-  # end
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
 
@@ -36,7 +26,6 @@ class User < ApplicationRecord
         user_name: auth.info.name,
         )
     end
-
     user
    end
 end
