5c74862aeaf5ef03f4446e395227867cca094d63
Nozomi <nozomi@nozomi.local>
2019-04-17 14:05:10 +0900

    Add google login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,6 +8,29 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def twitter
   # end
 
+  def google_oauth2
+    callback_from(:google)
+  end
+
+  # コールバック
+  private
+  def callback_from(provider)
+    provider = provider.to_s
+
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      print("persisted true")
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      print("persisted false")
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to controller: 'sessions', action: 'new'
+    end
+  end
+
+
   # More info at:
   # https://github.com/plataformatec/devise#omniauth
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'roots#top'
   devise_for :users, :controllers => {
-    :registrations => 'users/registrations'
+    :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'users/registrations'
   }
   get '/admin_top', to: 'roots#admin_top'
   resources :events, except: [:destroy] do
